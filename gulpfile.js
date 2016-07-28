9098a97cc29a1118491d93e0bf42aaf9580e8203
TedYuen <tedyuen.goo@gmail.com>
2016-07-28 17:16:11 +0800

    Modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,30 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var browserify = require('gulp-browserify');
 var obfuscate = require('gulp-obfuscate');
-var minifycss = require('gulp-minify-css');
-
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
 
 //---- sass
-gulp.task('sass', function () {
+gulp.task('sass',['delCss'], function () {
   return gulp.src('./app/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./app/build/css'))
+    .pipe(gulp.dest('./app/sass/css'));
+  gulp.run('concatcss');
+  gulp.run('minify-css');
 });
 
-gulp.task('minifycss', function() {
-  return gulp.src('./app/build/css/*.css')      //压缩的文件
-    .pipe(gulp.dest('./app/build/css'))   //输出文件夹
-    .pipe(minifycss());   //执行压缩
+gulp.task('concatcss',['sass'],function(){
+  return gulp.src(['./app/sass/css/*.css'])    //- 需要处理的css文件，放到一个字符串数组里
+    .pipe(concat('all.min.css'))
+    .pipe(gulp.dest('./app/sass/css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/sass/**/*.scss', ['sass','minifycss']);
+gulp.task('minify-css',['concatcss'], function() {
+  return gulp.src('./app/sass/css/all.min.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./app/build/css'));
 });
+
 //---- sass
 
 
@@ -62,5 +67,14 @@ gulp.task('js-build',function(){
 });
 
 //---- js
+
+//--del
+gulp.task('delCss',function(){
+  del([
+    './app/sass/css/*.*',
+    './app/build/css/all.min.css'
+  ]);
+});
+//--del
 
 gulp.task('default',['nodeModule','js-build','sass']);
